9ed387b4b7219d1d6100952dac5a4e216cbf4e2b
GitHub Action <action@github.com>
2022-10-08 06:36:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/hone1st/laravelbuildcode/MyBundle.kt
+++ src/main/kotlin/com/github/hone1st/laravelbuildcode/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hone1st.laravelbuildcode
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hone1st/laravelbuildcode/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hone1st/laravelbuildcode/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hone1st.laravelbuildcode.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hone1st.laravelbuildcode.MyBundle
 
 class MyApplicationService {
 
